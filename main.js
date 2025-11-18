2e4150012bd29ccfc8ac2f7c18b5926835d6886d
Paul Smith <paulsmithkc@gmail.com>
2025-11-18 11:10:00 -0600

    d20 with wireframe

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
 import * as THREE from 'three'
+import { Line2 } from 'three/examples/jsm/lines/Line2.js'
+import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js'
+import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js'
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xe8d4b8)
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
+camera.position.z = 5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setAnimationLoop(animate)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const geometry = new THREE.IcosahedronGeometry()
+const solidMaterial = new THREE.MeshBasicMaterial({
+  color: 0xcc0000,
+})
 
-camera.position.z = 5
+const solidPositions = geometry.attributes.position
+const wireframePositions = []
+for (let i = 0; i < solidPositions.count; i++) {
+  wireframePositions.push(
+    solidPositions.getX(i),
+    solidPositions.getY(i),
+    solidPositions.getZ(i)
+  )
+}
+const wireframeGeometry = new LineGeometry()
+wireframeGeometry.setPositions(wireframePositions)
+const wireframeMaterial = new LineMaterial({
+  color: 0x000000,
+  linewidth: 3,
+})
+wireframeMaterial.resolution.set(window.innerWidth, window.innerHeight)
+
+const solidMesh = new THREE.Mesh(geometry, solidMaterial)
+const wireframeMesh = new Line2(wireframeGeometry, wireframeMaterial)
+scene.add(solidMesh)
+scene.add(wireframeMesh)
 
 function animate(time) {
-  cube.rotation.x = time * 0.001
-  cube.rotation.y = time * 0.001
+  solidMesh.rotation.x = time * 0.001
+  solidMesh.rotation.y = time * 0.001
+  wireframeMesh.rotation.x = time * 0.001
+  wireframeMesh.rotation.y = time * 0.001
   renderer.render(scene, camera)
 }
 
@@ -30,4 +57,5 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
+  wireframeMaterial.resolution.set(window.innerWidth, window.innerHeight)
 })
